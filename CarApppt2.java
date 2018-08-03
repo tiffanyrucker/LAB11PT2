e8a3ab28fac2cf2eafb3741777c3d030283c80a1
tiffany <tiffanyrucker@gmail.com>
2018-08-03 16:30:44 -0400

    seventh commit

--- CarApppt2.java
+++ CarApppt2.java
@@ -6,16 +6,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Nadun
- */
+/
 public class CarApp {
     
     // input scanner 
